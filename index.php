9283618db025f7c8cdd6db8367bf274842139765
ashwinigit123 <ashwinitrale8@gmail.com>
2023-03-13 09:10:29 -0500

    removed second message

--- index.php
+++ index.php
@@ -128,7 +128,6 @@
             <h2>Application<br> Login Page</h2>
             <p>Login or register from here to access.</p>
             <p>This is a change</p>
-            <p> second change</p>
          </div>
       </div>
       <div class="main">
